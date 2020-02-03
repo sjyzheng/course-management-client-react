aae54220c5b0a38dee39fcd0ee215ecf7fec2ef0
sjyzheng <zjysteve@gmail.com>
2020-02-02 23:32:51 -0800

    Create CourseEditorComponent.js

---
+++ src/components/CourseEditorComponent.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+import LessonTabsComponent from "./LessonTabsComponent";
+import ModuleListComponent from "./ModuleListComponent";
+import TopicPillsComponent from "./TopicPillsComponent";
+import WidgetListComponent from "./WidgeListComponent";
+
+const CourseEditorComponent = () => (
+    <div>
+        <div className="container-fluid p-0">
+            <LessonTabsComponent/>
+        </div>
+
+        <div className="container-fluid p-0">
+            <div className="row">
+                <div className="col-3 d-none d-lg-block">
+                    <ModuleListComponent/>
+                </div>
+                    <div className="col-lg-9 col-12">
+                        <div className="d-none d-md-block">
+                            <TopicPillsComponent/>
+                        </div>
+                        <WidgetListComponent/>
+                </div>
+            </div>
+        </div>
+    </div>
+)
+
+export default CourseEditorComponent

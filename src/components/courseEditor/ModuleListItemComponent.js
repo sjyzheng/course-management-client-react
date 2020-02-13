cdb6e365742381d53c94998d75cc47e0dbf9f6ed
sjyzheng <zjysteve@gmail.com>
2020-02-12 16:50:11 -0800

    Update ModuleListItemComponent.js

--- src/components/courseEditor/ModuleListItemComponent.js
+++ src/components/courseEditor/ModuleListItemComponent.js
@@ -51,11 +51,11 @@ class ModuleListItemComponent extends React.Component {
                 {!this.state.editing &&
                     <div className="row" data-toggle="tooltip" data-placement="right" title={this.state.moduleTitle}>
                         <Link to={`/courses/${this.props.courseId}/modules/${this.state.moduleId}/lessons/lessonList/topics/topicList`}
-                              className="text-white text-truncate wbdv-module-item-title ml-2 col-7 "
+                              className="text-white text-truncate wbdv-module-item-title ml-md-2 col-7 "
                               key={this.state.moduleId}>
                             {this.state.moduleTitle}
                         </Link>
-                        <i className="fas fa-pencil-alt text-right mt-1 ml-2 wbdv-module-item-delete-btn col" onClick={(e) => {
+                        <i className="fas fa-pencil-alt text-right mt-1 ml-md-2 wbdv-module-item-delete-btn col" onClick={(e) => {
                             e.stopPropagation();
                             this.editModule();
                         }}> </i>
@@ -70,7 +70,7 @@ class ModuleListItemComponent extends React.Component {
                                placeholder="Enter a New Title"
                                onChange={this.updateForm}/>
                        <div className="col">
-                           <i className="fas fa-check mt-2 ml-2"
+                           <i className="fas fa-check mt-2 ml-md-2"
                               onClick={()=>this.saveModule(this.props.module)}>
                            </i>
                            <i className="far fa-trash-alt float-right mt-2"

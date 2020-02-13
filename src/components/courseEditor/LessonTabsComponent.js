96653625f5db8ec42375125c08822190529f4e0a
sjyzheng <zjysteve@gmail.com>
2020-02-12 16:07:51 -0800

    Create LessonTabsComponent

---
+++ src/components/courseEditor/LessonTabsComponent.js
@@ -0,0 +1,80 @@
+import React from "react";
+import LessonTabsItemComponent from "./LessonTabsItemComponent";
+
+class LessonTabsComponent extends React.Component {
+    componentDidMount() {
+        this.props.findLessonsForModule(this.props.moduleId);
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        if(this.props.moduleId !== prevProps.moduleId) {
+            this.props.findLessonsForModule(this.props.moduleId)
+        }
+    }
+
+    render() {
+        return (
+            <div className="nav nav-tabs nav-fill">
+                {this.props.lessons && this.props.lessons.map(lesson =>
+                    <LessonTabsItemComponent
+                        lesson = {lesson}
+                        key = {lesson._id}
+                        courseId = {this.props.courseId}
+                        moduleId = {this.props.moduleId}
+                        deleteLesson = {this.props.deleteLesson}
+                        updateLesson = {this.props.updateLesson}
+                        params = {this.props.params}
+                        history = {this.props.history}
+                    />
+                )}
+
+                <div className="nav-item">
+                    <div className="nav-link"
+                       onClick={() => {
+                           this.props.createLesson(this.props.moduleId, {title: 'New Lesson'})
+                       }}>
+                        Add Lesson
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+// const LessonTabsComponent = ({course}) => (
+//     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+//         <div>
+//             <Link to="/" className="fas fa-times wbdv-course-editor wbdv-close"> </Link>
+//             <i className="navbar-brand col-3 mr-sm-3 font-weight-bold">
+//                 {course.title}</i>
+//         </div>
+//
+//         <div className="collapse navbar-collapse justify-content-center" >
+//             <div className="navbar-nav list-group list-group-horizontal col-10">
+//                 <i className="nav-item list-group-item list-group-item-action list-group-item-light border-0 font-weight-bold bg-dark active px-4 text-center wbdv-page-tab"
+//                     > Lesson 1</i>
+//                 <i className="nav-item list-group-item list-group-item-action list-group-item-light border-0 font-weight-bold bg-dark px-4 text-center wbdv-page-tab"
+//                     > Lesson 2</i>
+//                 <i className="nav-item list-group-item list-group-item-action list-group-item-light border-0 font-weight-bold bg-dark px-4 text-center wbdv-page-tab"
+//                     > Lesson 3</i>
+//                 <i className="nav-item list-group-item list-group-item-action list-group-item-light border-0 font-weight-bold bg-dark px-4 text-center wbdv-page-tab"
+//                     > Lesson 4</i>
+//                 <i className="nav-item list-group-item list-group-item-action list-group-item-light border-0 font-weight-bold bg-dark px-4 text-center wbdv-page-tab"
+//                     > Lesson 5</i>
+//
+//             </div>
+//         </div>
+//         <div>
+//             <i className="fas fa-plus collapse navbar-collapse wbdv-new-page-btn"> </i>
+//         </div>
+//         <div>
+//             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+//                     aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+//                 <span className="navbar-toggler-icon"> </span>
+//             </button>
+//         </div>
+//     </nav>
+// )
+
+
+export default LessonTabsComponent
---
+++ src/components/courseEditor/LessonTabsItemComponent.js
@@ -0,0 +1,93 @@
+import React from "react";
+import {Link} from "react-router-dom";
+
+class LessonTabsItemComponent extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            active: false,
+            editing: false,
+            lessonId: this.props.lesson._id,
+            lessonTitle: this.props.lesson.title
+        }
+    }
+
+    selectLesson = () => {
+        this.setState(prevState => ({
+            editing: false,
+            lessonTitle: this.state.lessonTitle
+        }))
+    }
+
+    editLesson = () => {
+        this.setState(prevState => ({
+            active: true,
+            editing: !prevState.editing
+        }))
+    }
+
+    saveLesson = (lesson) =>{
+        // console.log(module);
+        if (this.state.lessonTitle === '') {
+            alert('Please Enter a Valid Lesson Title!');
+        } else {
+            this.setState(prevState => ({
+                editing: !prevState.editing,
+                active: !prevState.active
+            }));
+            lesson.title=this.state.lessonTitle;
+            this.props.updateLesson(lesson._id, lesson)
+        }
+    }
+
+    updateForm = (e) => {
+        this.setState({
+            lessonTitle: e.target.value
+        })
+    }
+
+    render() {
+        return (
+            <li className={`nav-item wbdv-lesson-item`}>
+                {!this.state.editing &&
+                    <div className={`nav-link mx-1 ${(this.state.lessonId === this.props.params.lessonId || this.state.active)?'active':''}`} >
+                        <Link to={`/courses/${this.props.courseId}/modules/${this.props.moduleId}/lessons/${this.state.lessonId}/topics/topicList`}
+                              className="text-truncate wbdv-module-item-title text-dark"
+                              key={this.state.lessonId}>
+                            {this.state.lessonTitle}
+                        </Link>
+                        <i className="fas fa-pencil-alt text-right mt-1 ml-1 wbdv-lesson-item-edit-btn" onClick={(e) => {
+                            e.stopPropagation();
+                            this.editLesson();
+                        }}> </i>
+                    </div>
+                }
+
+
+                {this.state.editing &&
+                    <div className={`nav-link mx-1 form-inline ${(this.state.lessonId === this.props.params.lessonId || this.state.active)?'active':''}`}>
+                        <input className="wbdv-lesson-editFld form-control mr-1"
+                               type="search"
+                               value={this.state.lessonTitle}
+                               placeholder="Enter a New Title"
+                               onChange={this.updateForm}/>
+                        <i className="fas fa-check"
+                           onClick={() => this.saveLesson(this.props.lesson)}>
+                        </i>
+                        <i className="far fa-trash-alt mx-2"
+                           onClick={() => {
+                               this.props.deleteLesson(this.state.lessonId);
+                               console.log(this.state.lessonId)
+                               if (this.state.active) {
+                                   this.props.history.push(`/courses/${this.props.courseId}/modules/${this.props.moduleId}/lessons/lessonList/topics/topicList`)
+                               }
+                           }}>
+                        </i>
+                    </div>
+                }
+            </li>
+        );
+    }
+}
+
+export default LessonTabsItemComponent;

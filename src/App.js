36b441367f9c2f414cf82fd9ca4b9511515bb44d
sjyzheng <zjysteve@gmail.com>
2020-02-12 16:03:19 -0800

    Add router to course-manager and course-editor

--- src/App.js
+++ src/App.js
@@ -4,17 +4,16 @@ import {BrowserRouter as Router, Link, Route}
     from 'react-router-dom'
 import './styles/CourseList.style.client.css'
 import CourseManagerContainer from "./containers/CourseManagerContainer";
-import CourseEditorComponent from "./components/CourseEditorComponent"
+import CourseEditorComponent from "./components/courseEditor/CourseEditorComponent"
 
 
 const App = () => (
-
     <Router>
         <div className="Page">
             <Route path="/" component={CourseManagerContainer} exact/>
-            <Route path="/course" component={CourseEditorComponent}/>
+            <Route path="/courses/:courseId/modules/:moduleId/lessons/:lessonId/topics/:topicId" component={CourseEditorComponent}/>
         </div>
     </Router>
-    )
+)
 
 export default App;

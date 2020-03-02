c694de4ed3a3c24803d4e0bb723463b11d886427
Steve <i.stevezhengjy@gmail.com>
2020-03-01 22:23:05 -0700

    Merge WidgetListComponent into WidgetListContainer

--- src/containers/WidgetListContainer.js
+++ src/containers/WidgetListContainer.js
@@ -1,16 +1,88 @@
 import React from "react";
 import {connect} from "react-redux";
 import WidgetService from "../services/WidgetService";
-import WidgetListComponent from "../components/courseEditor/WidgeListComponent";
-import {
-    createWidget,
-    deleteWidget,
-    findWidgetsForTopic,
-    moveDownWidget,
-    moveUpWidget,
-    updateWidget,
-    previewWidgets
-} from "../actions/widgetActions";
+import {createWidget, deleteWidget, findWidgetsForTopic, moveDownWidget, moveUpWidget, updateWidget, previewWidgets}from "../actions/widgetActions";
+import Switch from "react-switch";
+import WidgetListItemComponent from "../components/courseEditor/WidgetListItemComponent";
+
+class WidgetListContainer extends React.Component {
+    componentDidMount() {
+        this.props.findWidgetsForTopic(this.props.topicId);
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        if(this.props.topicId !== prevProps.topicId) {
+            this.props.findWidgetsForTopic(this.props.topicId);
+            this.setState({previewing: false})
+        }
+    }
+
+    state = {
+        editing: false,
+        previewing: false,
+        topicId: this.props.topicId,
+    }
+
+    previewWidgets = () => {
+        this.setState(prevState => ({
+            previewing: !prevState.previewing
+        }));
+    };
+
+    render(){
+        return (
+            <div>
+                <div>
+                    {this.props.widgets.length !== 0 &&
+                    <div className="row justify-content-end mt-2 mr-3">
+                        <label className="mr-3" htmlFor="switchControl">Preview</label>
+                        <Switch id="switchControl" onChange={this.previewWidgets} checked={this.state.previewing} />
+                    </div>
+                    }
+                </div>
+
+                <div>
+                    {this.props.widgets && this.props.widgets
+                        // .sort((a,b) => {
+                        //     if (a.order > b.order) return 1;
+                        //     else return -1;
+                        // })
+                        .map(widget =>
+                            <div key={widget.id}>
+                                <WidgetListItemComponent
+                                    widgets = {this.props.widgets}
+                                    widget = {widget}
+                                    courseId = {this.props.courseId}
+                                    moduleId = {this.props.moduleId}
+                                    lessonId = {this.props.lessonId}
+                                    topicId = {this.state.topicId}
+                                    editing = {this.state.editing}
+                                    previewing = {this.state.previewing}
+                                    deleteWidget = {this.props.deleteWidget}
+                                    updateWidget = {this.props.updateWidget}
+                                    moveUpWidget = {this.props.moveUpWidget}
+                                    moveDownWidget = {this.props.moveDownWidget}
+                                    params = {this.props.params}
+                                    history = {this.props.history}
+                                />
+                            </div>
+                        )}
+                </div>
+
+
+                <div className="fas fa-plus-circle fa-2x float-right mr-2 wbdv-button wbdv-add-course"
+                     onClick={()=>{
+                         this.props.createWidget(this.props.topicId,
+                             {text: 'New Widget',
+                                 id: (new Date()).getTime()+"",
+                             })
+                     }}>
+                </div>
+            </div>
+        )
+    }
+}
+
 
 
 const widgetService = new WidgetService();
@@ -48,9 +120,7 @@ const dispatchToPropertyMapper = (dispatch) => ({
         dispatch(moveDownWidget(widgetId)),
 
 });
-const WidgetListContainer = connect(
+export default connect(
     stateToPropertyMapper,
     dispatchToPropertyMapper
-)(WidgetListComponent);
-
-export default WidgetListContainer
+)(WidgetListContainer);

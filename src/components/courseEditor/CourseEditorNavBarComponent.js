3e80d15f6d4d0a77c53d38762841f086f6df0605
sjyzheng <zjysteve@gmail.com>
2020-03-01 22:26:04 -0700

    Make NavBarComponent stateless

--- src/components/courseEditor/CourseEditorNavBarComponent.js
+++ src/components/courseEditor/CourseEditorNavBarComponent.js
@@ -1,31 +1,13 @@
 import React from "react";
-import CourseService from "../../services/CourseService";
 import {Link} from "react-router-dom";
 
-class CourseEditorNavBarComponent extends React.Component {
-    state ={
-        title: " ",
-    };
-
-    componentDidMount() {
-        new CourseService().findCourseById(this.props.courseId)
-            .then((course)=>{
-                this.setState({title: course.title})
-            });
-    }
-
-    render() {
-        return (
-            <nav className="navbar navbar-dark bg-dark row">
-                <Link to={"/table"} className="fas fa-times fa-lg wbdv-close mt-1 col-1 text-center" style={{color: 'white'}}>
-                </Link>
-                <div className="navbar-brand font-weight-bold wbdv-course-editor-title col">
-                    {this.state.title}
-                </div>
-            </nav>
-
-        );
-    }
-}
+const CourseEditorNavBarComponent = ({courseTitle}) =>
+    <nav className="navbar navbar-dark bg-dark row">
+        <Link to={"/table"} className="fas fa-times fa-lg wbdv-close mt-1 col-1 text-center" style={{color: 'white'}}>
+        </Link>
+        <div className="navbar-brand font-weight-bold wbdv-course-editor-title col">
+            {courseTitle}
+        </div>
+    </nav>
 
 export default CourseEditorNavBarComponent
--- src/containers/CourseEditorContainer.js
+++ src/containers/CourseEditorContainer.js
@@ -1,6 +1,5 @@
 import React from "react";
 
-import WidgetListComponent from "../components/courseEditor/WidgeListComponent";
 import {Provider} from "react-redux";
 import moduleReducer from "../reducers/moduleReducer";
 import lessonReducer from "../reducers/lessonReducer";
@@ -9,78 +8,85 @@ import widgetReducer from "../reducers/widgetReducer";
 import ModuleListContainer from "./ModuleListContainer";
 import {combineReducers, createStore} from "redux";
 import LessonTabsContainer from "./LessonTabsContainer";
-import CourseEditorNavBarComponent from "../components/courseEditor/CourseEditorNavBarComponent";
 import TopicPillsContainer from "./TopicPillsContainer";
 import WidgetListContainer from "./WidgetListContainer";
-// import CourseEditorDropdownsContainer from "../../containers/CourseEditorDropdownsContainer";
-// import CourseEditorDropdownsComponent from "./CourseEditorDropdownsComponent";
+import CourseService from "../services/CourseService";
+import CourseEditorNavBarComponent from "../components/courseEditor/CourseEditorNavBarComponent";
 
 const rootReducer = combineReducers({
     moduleReducer,
     lessonReducer,
     topicReducer,
-    widgetReducer
+    widgetReducer,
 })
 
 const store = createStore(rootReducer);
 
-const CourseEditorContainer = ({match, history}) =>
-    <Provider store={store}>
-        <div>
-            <div className="container-fluid p-0">
-                <CourseEditorNavBarComponent
-                    courseId = {match.params.courseId}
-                    history = {history}
-                />
-                {/*<CourseEditorDropdownsContainer*/}
-                {/*    courseId = {match.params.courseId}*/}
-                {/*    moduleId = {match.params.moduleId}*/}
-                {/*    lessonId = {match.params.lessonId}*/}
-                {/*    history = {history}*/}
-                {/*    params = {match.params}*/}
-                {/*/>*/}
-            </div>
+class CourseEditorContainer extends React.Component {
 
-            <div className="container-fluid p-0">
-                <div className="row">
-                    <div className="col-3">
-                        <ModuleListContainer
-                            courseId = {match.params.courseId}
-                            history = {history}
-                            params = {match.params}
+    state = {
+        courseTitle: ""
+    };
+
+    componentDidMount() {
+        new CourseService().findCourseById(this.props.match.params.courseId)
+            .then((course)=>{
+                this.setState({courseTitle: course.title})
+            });
+    }
+
+    render() {
+        return (
+            <Provider store={store}>
+                <div>
+                    <div className="container-fluid p-0">
+                        <CourseEditorNavBarComponent
+                            courseTitle = {this.state.courseTitle}
                         />
                     </div>
-                    <div className="col-9">
-                        <div className="my-2">
-                            <LessonTabsContainer
-                                courseId = {match.params.courseId}
-                                moduleId = {match.params.moduleId}
-                                history = {history}
-                                params = {match.params}/>
-                        </div>
 
-                        <div>
-                            <TopicPillsContainer
-                                    courseId = {match.params.courseId}
-                                    moduleId = {match.params.moduleId}
-                                    lessonId = {match.params.lessonId}
-                                    history = {history}
-                                    params = {match.params}/>
-                        </div>
-                        <div>
-                            {match.params.topicId !== undefined &&
-                            <WidgetListContainer
-                                courseId = {match.params.courseId}
-                                moduleId = {match.params.moduleId}
-                                lessonId = {match.params.lessonId}
-                                topicId = {match.params.topicId}
-                                history = {history}
-                                params = {match.params}/>}
+                    <div className="container-fluid p-0">
+                        <div className="row">
+                            <div className="col-3">
+                                <ModuleListContainer
+                                    courseId = {this.props.match.params.courseId}
+                                    params = {this.props.match.params}
+                                />
+                            </div>
+                            <div className="col-9">
+                                <div className="my-2">
+                                    <LessonTabsContainer
+                                        courseId = {this.props.match.params.courseId}
+                                        moduleId = {this.props.match.params.moduleId}
+                                        params = {this.props.match.params}
+                                        history = {this.props.history}/>
+
+                                </div>
+
+                                <div>
+                                    <TopicPillsContainer
+                                        courseId = {this.props.match.params.courseId}
+                                        moduleId = {this.props.match.params.moduleId}
+                                        lessonId = {this.props.match.params.lessonId}
+                                        params = {this.props.match.params}
+                                        history = {this.props.history}/>
+                                </div>
+                                <div>
+                                    {this.props.match.params.topicId !== undefined &&
+                                    <WidgetListContainer
+                                        courseId = {this.props.match.params.courseId}
+                                        moduleId = {this.props.match.params.moduleId}
+                                        lessonId = {this.props.match.params.lessonId}
+                                        topicId = {this.props.match.params.topicId}
+                                        params = {this.props.match.params}
+                                        history = {this.props.history}/>}
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </div>
-    </Provider>
-
+            </Provider>
+        );
+    }
+}
 export default CourseEditorContainer

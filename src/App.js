eba86479da2dee335672baf91e0a984af2d102a7
Steve <i.stevezhengjy@gmail.com>
2020-03-01 22:18:41 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import React from 'react';
 // import './App.css';
-import {BrowserRouter as Router, Link, Route}
-    from 'react-router-dom'
+
 import './styles/CourseList.style.client.css'
 import CourseManagerContainer from "./containers/CourseManagerContainer";
-import CourseEditorContainer from "./containers/CourseEditorContainer"
-
 
 const App = () =>
     <CourseManagerContainer/>

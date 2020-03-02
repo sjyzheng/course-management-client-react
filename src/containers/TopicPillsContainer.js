a451220b3633d025dea16ed524a542acba19c35c
Steve <i.stevezhengjy@gmail.com>
2020-03-01 22:21:59 -0700

    Merge TopicListComponent into TopicListContainer

--- src/containers/TopicPillsContainer.js
+++ src/containers/TopicPillsContainer.js
@@ -1,9 +1,50 @@
 import React from "react";
 import {connect} from "react-redux";
 import {findTopicsForLesson, deleteTopic, updateTopic, createTopic} from "../actions/topicActions";
-import TopicPillsComponent from "../components/courseEditor/TopicPillsComponent";
 import TopicService from "../services/TopicService";
+import TopicPillsItemComponent from "../components/courseEditor/TopicPillsItemComponent";
 
+class TopicPillsContainer extends React.Component {
+    componentDidMount() {
+        this.props.findTopicsForLesson(this.props.lessonId);
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        if(this.props.lessonId !== prevProps.lessonId) {
+            this.props.findTopicsForLesson(this.props.lessonId)
+        }
+    }
+
+    render() {
+        return (
+            <div className="nav nav-pills nav-fill my-2 text-nowrap wbdv-topic-pill-list">
+                {this.props.topics && this.props.topics.map(topic =>
+                    <TopicPillsItemComponent
+                        topic = {topic}
+                        key = {topic._id}
+                        courseId = {this.props.courseId}
+                        moduleId = {this.props.moduleId}
+                        lessonId = {this.props.lessonId}
+                        deleteTopic = {this.props.deleteTopic}
+                        updateTopic = {this.props.updateTopic}
+                        params = {this.props.params}
+                        history = {this.props.history}
+                    />
+                )}
+
+                {this.props.lessonId &&
+                <div className={`nav-item`}>
+                    <div className="nav-link bg-light mx-1 wbdv-topic-add-btn"
+                         onClick={()=>{
+                             this.props.createTopic(this.props.lessonId, {title: 'New Topic'})
+                         }}>
+                        Add Topic
+                    </div>
+                </div>}
+            </div>
+        )
+    }
+}
 
 const topicService = new TopicService();
 
@@ -32,9 +73,7 @@ const dispatchToPropertyMapper = (dispatch) => ({
                 dispatch(updateTopic(topic, topicId))
             )
 });
-const TopicPillsContainer = connect(
+export default connect(
     stateToPropertyMapper,
     dispatchToPropertyMapper
-)(TopicPillsComponent);
-
-export default TopicPillsContainer
+)(TopicPillsContainer);

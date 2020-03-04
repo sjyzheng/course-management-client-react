865eec64cb3c1068688d643d94e1f112a5d84247
sjyzheng <zjysteve@gmail.com>
2020-03-04 14:22:31 -0700

    Minor changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 // import './App.css';
 
-import './styles/CourseList.style.client.css'
+import './styles/CourseManager.style.client.css'
 import CourseManagerContainer from "./containers/CourseManagerContainer";
 
 const App = () =>
